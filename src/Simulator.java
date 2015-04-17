6c55e0bbcf5454cb6a45d458a85b0c6d38e249eb
Denver Fernandes <sub367@gmail.com>
2015-04-17 17:42:10 +0100

    Added support for the changing of the number of servers and the length of simulation. Also adjusted the length of each tick.

--- src/Simulator.java
+++ src/Simulator.java
@@ -9,17 +9,30 @@ import java.util.concurrent.TimeUnit;
 
 public class Simulator {
 	
-	public static long TICK = 2;
+	public static long TICK = 10;
+	public static int NUM_OF_SERVERS = 3;
+	public static int LENGTH_OF_SIMULATION = 7200; //should equal 4 hours in simulated time unless I have my math wrong :s
 	
 	public static void main(String[] args) {
-		MultiQueueControlSystem.getInstance().generateQueuesAndServers(3);
 		
-		SingleQueueControlSystem.getInstance().generateQueuesAndServers(3);
+		MultiQueueControlSystem.getInstance().generateQueuesAndServers(NUM_OF_SERVERS);
+		SingleQueueControlSystem.getInstance().generateQueuesAndServers(NUM_OF_SERVERS);
+		
         final ScheduledExecutorService ticker = Executors.newSingleThreadScheduledExecutor();
+        
         ticker.scheduleAtFixedRate(new Runnable(){
-
+        	
+        	long currentTime = 0;
+        	
 			@Override
 			public void run() {
+				if(currentTime < LENGTH_OF_SIMULATION) {
+					currentTime = currentTime + 10;
+				} else {
+					ticker.shutdown();
+					return;
+				}
+				
             	MultiQueueControlSystem.getInstance().customerArrival();
             	MultiQueueControlSystem.getInstance().allocateCustomersToServers();
             	MultiQueueControlSystem.getInstance().removeFinishedCustomersFromServers();
@@ -27,9 +40,10 @@ public class Simulator {
             	SingleQueueControlSystem.getInstance().customerArrival();
             	SingleQueueControlSystem.getInstance().allocateCustomersToServers();
             	SingleQueueControlSystem.getInstance().removeFinishedCustomersFromServers();
+            	
 			}
-        	
         }, 0, TICK, TimeUnit.SECONDS);
+        
 	}
 
 }
