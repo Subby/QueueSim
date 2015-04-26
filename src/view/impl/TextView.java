72ffb6aa177aa42d0c2c0065bc30eaa6d22c3ee4
Denver Fernandes <sub367@gmail.com>
2015-04-26 13:39:53 +0100

    Forgot about this. TextView added, bit messy though.

--- src/view/impl/TextView.java
+++ src/view/impl/TextView.java
@@ -1,6 +1,10 @@
 package view.impl;
 
+import java.util.Scanner;
+
+import model.MultiQueueControlSystem;
 import model.QueueControlSystem;
+import model.Simulator;
 import model.SingleQueueControlSystem;
 import model.Stats;
 import view.SimulatorView;
@@ -14,8 +18,24 @@ import view.SimulatorView;
 
 public class TextView implements SimulatorView {
 
-	private QueueControlSystem selectedQueueControlSystem = SingleQueueControlSystem.getInstance();
-	private Stats stats = selectedQueueControlSystem.getStats();
+	private static QueueControlSystem selectedQueueControlSystem;
+	private static int numOfservers;
+	private static int lenOfSimulation;
+	private static Simulator simulator;
+	private static Stats stats;
+	private static boolean finished = false;
+	
+	public static void main(String... args) {
+		simulator = new Simulator();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the queue simulator");
+        while(!finished) {
+        	System.out.print("> ");
+            parseInput(input);
+        }
+        input.close();
+        System.exit(0);
+	}
 	
 	@Override
 	public String output() {
@@ -28,8 +48,50 @@ public class TextView implements SimulatorView {
 
 	}
 	
-	public void run(){
-		System.out.println(output());
+	/**
+	 * Parses the {@link Scanner} input.
+	 * @param input the input given by the user
+	 */
+	private static void parseInput(Scanner input){
+        System.out.println("What type of queue system would you like (sq - Single Queue System, mq - Multi Queue System, default - sq)?");
+        System.out.print("> ");
+        
+        switch(input.next()) {
+        case "sq":
+        	selectedQueueControlSystem = SingleQueueControlSystem.getInstance();
+        	break;
+        case "mq":
+        	selectedQueueControlSystem = MultiQueueControlSystem.getInstance();
+        	break;
+        default:
+        	System.out.println("Not a valid queue system, using the default queue system.");
+        	break;
+        }
+        
+        System.out.println("How many servers would you like?");
+        System.out.print("> ");
+        numOfservers = input.nextInt();
+        System.out.println("How long would you like the simulation to last (in hours)?");
+        System.out.print("> ");
+        lenOfSimulation = input.nextInt();
+        
+		stats = selectedQueueControlSystem.getStats();
+		simulator.setQueueSystem(selectedQueueControlSystem);
+		simulator.setNumberOfServers(numOfservers);
+		simulator.setLengthOfSimulation(lenOfSimulation);
+        simulator.main(null);
+        System.out.println(stats.toString());
+        
+        System.out.println("Would you like the statistics to be saved to a text file (y - yes, default - no)?");
+        switch(input.next()) {
+        case "y":
+        	System.out.println("Printing to file...");
+        	stats.outputToFile();
+        	System.out.println("Sucessfully prinited to file.");
+        	break;
+        }
+        System.out.println("Thank you for using this simulator");
+        finished = true;
 	}
 
 }
