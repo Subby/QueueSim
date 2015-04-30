76987567b9af8d6afe09bde7a96214282a11c588
lawtonbs <lawtonbs@eas-ubuntu-base>
2015-04-30 14:20:38 +0100

    (Almost) finish refactoring AnimatedView

--- src/view/impl/AnimatedView.java
+++ src/view/impl/AnimatedView.java
@@ -3,6 +3,7 @@ package view.impl;
 //Import application packages
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 import model.*;
@@ -20,7 +21,7 @@ public class AnimatedView extends JPanel implements SimulatorView {
 	
 	private static final long serialVersionUID = -34020607785964557L;
 	
-	private JFrame frame;
+	private static JFrame frame;
 	
 	private JLabel queuingSystemLabel;
 	private JComboBox<String> queueingSystemChoices;
@@ -28,51 +29,46 @@ public class AnimatedView extends JPanel implements SimulatorView {
     private JLabel numOfServersLabel;
 	private JComboBox<Integer> numOfServers;
 		
-	private JTextArea outputArea;
+	private final JTextArea outputArea;
 	private JScrollPane outputAreaScroll;
-	
-	private JButton runBtn;
-	private JButton cancelBtn;
-	
+		
     private LabelledSlider simulationLengthSlider;
     
+    private Simulator simulator;
     private Stats stats;
     
     private boolean simulatorRunning = false;
     
     public static void main(String[] args) {
-		final JFrame frame = new JFrame("Animated View");
+    	final JFrame frame = new JFrame("Animated View");
 		frame.getContentPane().add(new AnimatedView());
-		frame.setSize(800, 275);
+    	frame.setSize(800, 275);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.setVisible(true);
-		frame.addWindowListener(new WindowAdapter() {
-			  public void windowClosing(WindowEvent e) {
-				  int confirm = JOptionPane.showOptionDialog(frame,
-		                    "Are you sure you want to exit the simulator?",
-		                    "Exit Confirmation", JOptionPane.YES_NO_OPTION,
-		                    JOptionPane.QUESTION_MESSAGE, null, null, null);
-		            if (confirm == JOptionPane.OK_OPTION) {
-		                System.exit(0);
-		            }
-			  }
-			});
+    	frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				exitApp();
+			}
+		});
 	}
 
 	public AnimatedView() {
 		
 		// Create the components 
+		simulator = new Simulator();
+        stats = simulator.getQueueSystem().getStats();
 		JButton runBtn = new JButton();
 		JButton cancelBtn = new JButton("Cancel");
 		simulationLengthSlider = new LabelledSlider("Simulation Lengeth (hours) : ", 1, 1, 10, 1);
 		String[] queuingSystemItems = {"Single Queue", "Multiple Queue"};
         queueingSystemChoices = new JComboBox<String>(queuingSystemItems);
-        JTextArea outputArea = new JTextArea(13, 23);
-        JScrollPane outputAreaScroll = new JScrollPane();
+        outputArea = new JTextArea(13, 23);
+        outputAreaScroll = new JScrollPane();
         Integer[] numOfServersItems = {3, 4, 5, 6};
         numOfServersLabel = new JLabel ("Number of Servers");
         numOfServers = new JComboBox<Integer>(numOfServersItems);
+        queuingSystemLabel = new JLabel("Queueing System");
         
         // Set the properties of the components 
         runBtn.setText("Run");
@@ -85,8 +81,7 @@ public class AnimatedView extends JPanel implements SimulatorView {
         outputAreaScroll.setViewportView(outputArea);
         numOfServers.setSize(new Dimension(20, 20));
         
-        // Create containers to hold the components
-        frame = new JFrame("Animated View");
+        // Create containers to hold the components        
         JPanel westPanel = new JPanel();
         JPanel centerPanel = new JPanel();
         JPanel southPanel = new JPanel();
@@ -97,7 +92,6 @@ public class AnimatedView extends JPanel implements SimulatorView {
         JPanel queueTypePanel = new JPanel();
         
         // Specify LayoutManagers
-        frame.getContentPane().add(new AnimatedView());
         setLayout(new BorderLayout());
         sliderPanel.setLayout(new BorderLayout());
         comboPanel.setLayout(new BorderLayout());
@@ -110,112 +104,82 @@ public class AnimatedView extends JPanel implements SimulatorView {
         // Add components to the containers
         sliderPanel.add(simulationLengthSlider, BorderLayout.NORTH);
         comboPanel.add(serverPanel, BorderLayout.NORTH);
-        
-				
-		final Simulator simulator = new Simulator();
-        
-        stats = simulator.getQueueSystem().getStats();
-        
-        simulationLengthSlider = new LabelledSlider("Simulation Length (hours) : ", 1, 1, 10, 1);
-        
-        outputArea = new JTextArea(13, 23);
-        outputArea.setEditable(false);
-        outputAreaScroll = new JScrollPane();
-        outputAreaScroll.setViewportView(outputArea);
-        
-        //run button
-        runBtn = new JButton("Run");
-        runBtn.addActionListener(new ActionListener() {
-        	@Override
+        comboPanel.add(queueTypePanel, BorderLayout.SOUTH);
+        westPanel.add(comboPanel, BorderLayout.NORTH);
+		westPanel.add(sliderPanel, BorderLayout.SOUTH);
+		serverPanel.add(numOfServersLabel, BorderLayout.NORTH);
+		serverPanel.add(numOfServers, BorderLayout.SOUTH);
+		queueTypePanel.add(queuingSystemLabel, BorderLayout.NORTH);
+		queueTypePanel.add(queueingSystemChoices, BorderLayout.SOUTH);
+		centerPanel.add(outputAreaScroll);
+		buttonPanel.add(runBtn, BorderLayout.WEST);
+		buttonPanel.add(cancelBtn, BorderLayout.EAST);
+		southPanel.add(buttonPanel, BorderLayout.SOUTH);
+		add(westPanel, BorderLayout.WEST);
+		add(centerPanel, BorderLayout.CENTER);
+		add(southPanel, BorderLayout.SOUTH);
+		
+		// Arrange event handlers in the GUI
+		runBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (queueingSystemChoices.getSelectedItem().toString().equals("Single Queue")) {
-					simulator.setSingleQueueControlSystem();
-					stats = simulator.getQueueSystem().getStats();
-				}
-				else if (queueingSystemChoices.getSelectedItem().toString().equals("Multiple Queue")) {
-					simulator.setMultiQueueControlSystem();
-					stats = simulator.getQueueSystem().getStats();
-				}
-        		simulator.setShouldRun(true);
-        		simulator.setLengthOfSimulation((int)simulationLengthSlider.getValue());
-				simulator.run();
-				appenedToTextArea("Simulator running");
-				simulatorRunning = true;
-				//TODO implement error-handling for if JComboBox isn't working 
+				runSimulation();
 			}
-         });
-        
-        //cancel button
-        cancelBtn = new JButton("Cancel");
-        cancelBtn.addActionListener(new ActionListener() {
-			@Override
+		});
+		
+		cancelBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(!simulatorRunning) {
-					appenedToTextArea("Please run the simulator before cancelling it");
-					return;
+				appenedToTextArea("Please run the simulator before cancelling it");
+				return;
 				}
 				simulator.setShouldRun(false);
 				simulatorRunning = false;
 				appenedToTextArea("Simulator stopped");
 				outputArea.append(outputSimulationStats());
 			}
-        });
-        
-		//queue system select combo box
-        String[] queuingSystemItems = {"Single Queue", "Multiple Queue"};
-        queuingSystemLabel = new JLabel ("Queuing System");
-        queueingSystemChoices = new JComboBox<String>(queuingSystemItems);
-        
-        
-		//number of servers select combo box
-        Integer[] numOfServersItems = {3, 4, 5, 6};
-        numOfServersLabel = new JLabel ("Number of Servers");
-        numOfServers = new JComboBox<Integer>(numOfServersItems);
-        numOfServers.setSize(new Dimension(20, 20));
-        numOfServers.addActionListener(new ActionListener() {
+		});
 
-			@Override
+		
+		numOfServers.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int s = ((Integer) numOfServers.getSelectedItem());
-				simulator.setNumberOfServers(s);
+				setServers();
 			}
-        	
         });
-        
-		setLayout(new BorderLayout());
-		
-		sliderPanel.setLayout(new BorderLayout());
-		sliderPanel.add(simulationLengthSlider, BorderLayout.NORTH);
-		
-		comboPanel.setLayout(new BorderLayout());
-		comboPanel.add(serverPanel, BorderLayout.NORTH);
-		comboPanel.add(queueTypePanel, BorderLayout.SOUTH);
-				
-		westPanel.setLayout(new BorderLayout());
-		westPanel.add(comboPanel, BorderLayout.NORTH);
-		westPanel.add(sliderPanel, BorderLayout.SOUTH);
-		
-		serverPanel.setLayout(new BorderLayout());
-		serverPanel.add(numOfServersLabel, BorderLayout.NORTH);
-		serverPanel.add(numOfServers, BorderLayout.SOUTH);
-		
-		queueTypePanel.setLayout(new BorderLayout());
-		queueTypePanel.add(queuingSystemLabel, BorderLayout.NORTH);
-		queueTypePanel.add(queueingSystemChoices, BorderLayout.SOUTH);
-		
-		centerPanel.add(outputAreaScroll);
-		
-		buttonPanel.setLayout(new BorderLayout());
-		buttonPanel.add(runBtn, BorderLayout.WEST);
-		buttonPanel.add(cancelBtn, BorderLayout.EAST);
-		southPanel.setLayout(new BorderLayout());
-		southPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-		add(westPanel, BorderLayout.WEST);
-		add(centerPanel, BorderLayout.CENTER);
-		add(southPanel, BorderLayout.SOUTH);
 	}
-
+		
+	// Helper methods to aid event handler functionality 
+	
+	private void runSimulation() {
+		if (queueingSystemChoices.getSelectedItem().toString().equals("Single Queue")) {
+			simulator.setSingleQueueControlSystem();
+			stats = simulator.getQueueSystem().getStats();
+		}
+		else if (queueingSystemChoices.getSelectedItem().toString().equals("Multiple Queue")) {
+			simulator.setMultiQueueControlSystem();
+			stats = simulator.getQueueSystem().getStats();
+		}
+		simulator.setShouldRun(true);
+		simulator.setLengthOfSimulation((int)simulationLengthSlider.getValue());
+		simulator.run();
+		appenedToTextArea("Simulator running");
+		simulatorRunning = true;
+	}
+	
+	private static void exitApp() {
+		int confirm = JOptionPane.showOptionDialog(frame,
+				"Are you sure you want to exit the simulator?",
+				"Exit Confirmation", JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, null, null);
+		if (confirm == JOptionPane.OK_OPTION) {
+			System.exit(0);
+		}
+	}
+	
+	private void setServers() {
+		int s = ((Integer) numOfServers.getSelectedItem());
+		simulator.setNumberOfServers(s);
+	}
+	
 	/**
 	 * Appends a message to the output text area.
 	 * @param message the message to output.
